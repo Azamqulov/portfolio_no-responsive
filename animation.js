9afcf9d9f79d417f94530e14c1edce55033db04c
Azamqulov Musurmon <140374591+Azamqulov@users.noreply.github.com>
2023-09-08 13:36:06 +0500

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,16 @@
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute('data-dots')
+    var marked = elem.getAttribute('data-percent');
+    var percent = Math.floor(dots * marked / 100);
+    var rotate = 360 / dots;
+    var points = "";
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i: ${i}; --rot: ${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked')
+    }
+})
---
+++ script.js
@@ -0,0 +1,179 @@
+window.addEventListener('load',() =>{
+	document.querySelector('.block').classList.add('active')
+})
+
+const quyosh = document.querySelector('#sun')
+const oy = document.querySelector('#moon')
+const bodyy = document.querySelector('body')
+
+
+const btn = document.querySelector('.top-to');
+
+btn.addEventListener('click', () => {
+    scrollTo({
+        top: 0,
+        behavior: `smooth`,
+    });
+});
+window.addEventListener('scroll', () => {
+    if (pageYOffset > 100) {
+        btn.style.transform = `scale(1)`;
+    } else {
+        btn.style.transform = `scale(0)`;
+    }
+});
+
+// navbar   
+const burger = document.querySelector('.header-burger')
+const menu = document.querySelector('.header-menu')
+burger.addEventListener('click', () =>{
+    menu.classList.toggle('open');
+    burger.classList.toggle('active');
+})
+
+
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+ 
+ 	//Parallax            
+              
+	function scrollBanner() {
+	  $(document).on('scroll', function(){
+      var scrollPos = $(this).scrollTop();
+        $('.parallax-fade-top').css({
+          'top' : (scrollPos/2)+'px',
+          'opacity' : 1-(scrollPos/700)
+        });
+        $('.parallax-00').css({
+          'top' : (scrollPos/-3.5)+'px'
+        });
+        $('.parallax-01').css({
+          'top' : (scrollPos/-2.8)+'px'
+        });
+        $('.parallax-top-shadow').css({
+          'top' : (scrollPos/-2)+'px'
+        });
+      });    
+	  }
+	scrollBanner();	              
+
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }              
+              
+  
+
+
+
+	
+//Scroll back to top
+              
+$(document).ready(function() {	
+		var offset = 300;
+		var duration = 400;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.scroll-to-top').addClass('active-arrow');
+			} else {
+				jQuery('.scroll-to-top').removeClass('active-arrow');
+			}
+		});				
+		jQuery('.scroll-to-top').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+  
+  		
+		/* Hero Case study images */			
+		
+		$('.case-study-name:nth-child(1)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(1)').addClass("show");
+			$('.case-study-name:nth-child(1)').addClass('active');
+		})
+		$('.case-study-name:nth-child(2)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(2)').addClass("show");
+			$('.case-study-name:nth-child(2)').addClass('active');
+		})
+		$('.case-study-name:nth-child(3)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(3)').addClass("show");
+			$('.case-study-name:nth-child(3)').addClass('active');
+		})
+		$('.case-study-name:nth-child(4)').on('mouseenter', function() {
+			$('.case-study-name.active').removeClass('active');
+			$('.case-study-images li.show').removeClass("show");
+			$('.case-study-images li:nth-child(4)').addClass("show");
+			$('.case-study-name:nth-child(4)').addClass('active');
+		})
+		$('.case-study-name:nth-child(1)').trigger('mouseenter')
+  
+  });            
+              
+})(jQuery);
+
+
+
+
+const dots = document.querySelectorAll('.anim')
+
+prev.addEventListener('click', prevSlide)
+next.addEventListener('click', nextSlide)
+
+function RemoveActive() {
+	for (let i = 0; i < img.length; i++) {
+		dots[i].classList.remove('active')
+	}
+}
+
+dots.forEach(function (dot, index) {
+	dot.addEventListener('click', () => {
+		dotIndex = index
+		RemoveActive();
+
+		dot.classList.add('active')
+	})
+})
+
+function nextSlide() {
+	RemoveActive()
+	dots[count].classList.add('active')
+}
+
+
+function prevSlide() {
+	RemoveActive();
+	dots[count].classList.add('active');
+	
+}
